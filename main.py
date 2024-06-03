40446069bd076d48692a77e6a32ed6bd9ba09a83
kuldmivi <kuldmivi@gmail.com>
2024-06-04 00:05:51 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import re
+
+
+print('КАЛЬКУЛЯТОР')
+print('Программа для выполнения простых арифметических операций (+, -, *, /)')
+print('над целыми от 1 до 10')
+input_string = input('Введите строку содержащую выражение например "1 + 2":\n')
+
+
+def check_string(math_operation: str) -> str:
+    if re.fullmatch(r'^([1-9]|10) [-+*/] ([1-9]|10)$', math_operation.strip()):
+        return math_operation.replace(' ', '')
+    else:
+        raise ValueError
+
+
+def main(operation: str):
+    try:
+        print(int(eval(check_string(operation))))
+    except ValueError as e:
+        print('Строка не соотвествует формату. Попробуйте еще раз!')
+
+
+if __name__ == '__main__':
+    main(input_string)
