aada65f39c05a7e760f3db668b5458a8e20e4cf7
kuldmivi <kuldmivi@gmail.com>
2024-06-04 21:50:56 +0300

    изменено поведение функции main добавлен возврат значения, пернесен блок кода привествия

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import re
 
 
-print('КАЛЬКУЛЯТОР')
-print('Программа для выполнения простых арифметических операций (+, -, *, /)')
-print('над целыми от 1 до 10')
-input_string = input('Введите строку содержащую выражение например "1 + 2":\n')
-
-
 def check_string(math_operation: str) -> str:
     if re.fullmatch(r'^([1-9]|10) [-+*/] ([1-9]|10)$', math_operation.strip()):
         return math_operation.replace(' ', '')
@@ -14,12 +8,18 @@ def check_string(math_operation: str) -> str:
         raise ValueError
 
 
-def main(operation: str):
+def main(operation: str) -> int:
     try:
-        print(int(eval(check_string(operation))))
+        return int(eval(check_string(operation)))
     except ValueError as e:
         print('Строка не соотвествует формату. Попробуйте еще раз!')
 
 
 if __name__ == '__main__':
-    main(input_string)
+    print('КАЛЬКУЛЯТОР')
+    print('Программа для выполнения простых арифметических операций (+, -, *, /)')
+    print('над целыми от 1 до 10')
+    input_string = input('Введите строку содержащую выражение например "1 + 2":\n')
+    result = main(input_string)
+    if result is not None:
+        print(result)
